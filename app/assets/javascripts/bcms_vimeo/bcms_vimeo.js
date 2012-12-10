31d257e5894a2d688fecd87ad15bf5b3c05fb8c7
Justin Bogan <justin@backupify.com>
2012-12-10 10:13:02 -0500

    Remove unused javascripts.

--- app/assets/javascripts/bcms_vimeo/bcms_vimeo.js
+++
@@ -1,8 +0,0 @@
-$('document').on('click','.video-container a', function() {
-    alert('Here I am');
-    var videoID = $(this).attr('class');
-    var videoTitle = $(this).parent('article').children('h4').html();
-
-    $('#myModalLabel').html(videoTitle);
-    $('.modal-body iframe').attr('src','http://player.vimeo.com/video/' + videoID + '?byline=0&amp;portrait=0&amp;badge=0');
-});
